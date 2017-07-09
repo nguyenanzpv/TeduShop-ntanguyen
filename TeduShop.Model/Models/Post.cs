5be6c9885c2ca571e370e57cdc815eee17ef2657
AnNguyen-PC\AnNguyen <nguyenanzpv@gmail.com>
2017-07-09 20:27:00 +0700

    Commit ngay 09/07/17- Model

---
+++ TeduShop.Model/Models/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Abstracts;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Posts")]
+    public class Post:Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string Alias { set; get; }
+        [Required]
+        public int CategoryID { set; get; }
+        public string Image { set; get; }
+        public string Description { set; get; }
+        public string Content { set; get; }
+
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { set; get; }
+        public int? ViewCount { set; get; }
+
+        [ForeignKey("CategoryID")]
+        public virtual PostCategory PostCategory { set; get; }
+    }
+}
---
+++ TeduShop.Model/Models/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        [Required]
+        public string ID { set; get; }
+        public string Name { set; get; }
+        public string Type { set; get; }
+
+        public virtual IEnumerable<ProductTag> ProductTags { set; get; }
+    }
+}
